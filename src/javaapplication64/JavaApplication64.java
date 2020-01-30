05f44ff0b53d166c85e2646f053d40679ee1929e
ismael <ismael@serv1-t1.inf>
2020-01-30 14:11:48 +0100

    realización del método imprimir

---
+++ src/javaapplication64/JavaApplication64.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication64;
+import java.lang.*;
+/**
+ *
+ * @author ismael
+ */
+public class JavaApplication64 {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    static void imprimir(){
+        System.out.println("imprimir desarrollado");
+    }
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        imprimir();
+        
+        
+        
+    }
+    
+}
